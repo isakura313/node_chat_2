14683285df4cfc9b5fa7def9128d0fdc723fe110
Paul <isakura313@gmail.com>
2020-11-09 22:10:54 +0300

    asdad

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require("express")
-port = process.env.PORT || 8080;
+const express = require("express") // импортируем библиотку express
+port = process.env.PORT || 8080; // определение порта, с которого мы совершаем выход в интернет
 
-const app = express();
+const app = express(); // инициализируем экземпляр нашего приложения
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs")  // запускаем рендер нашего ejs шаблона
 
-app.use(express.static("public"))
+app.use(express.static("public")) //  статика, которая  отдается полностью неизменной
 
 app.get("/", (req, res)=>{
-    res.render("index")
+    res.render("index") // рендеринг  нашего ejs
 })
 
 server = app.listen(port, ()=>{
-    console.log("http://localhost:8080");
+    console.log("http://localhost:8080"); // адрес, на котором запускуется наш сервер
 })
-const io = require("socket.io")(server)
+const io = require("socket.io")(server) // подключаем socket.io к expresss
 // Какая функциональность будет в чате?
 // можно будет вписать имя и закрепить с собой
 // можно отправлять сообщения в чате
@@ -28,17 +28,19 @@ io.on("connection", (socket)=>{
 
 
 // смена имени
-socket.on("change_username", (data)=>{
-    console.log("Попытка поменять имя")
-    socket.username = data.username;
+socket.on("change_username", (data)=>{ // при emit change
+    console.log("Меняем имя")
+    socket.username = data.username; // задаем имя нашего пользователя
     console.log(`Имя пользователя было изменено на ${socket.username}`)
 })
 
 socket.on("new_message", (data)=>{
-    // if (data.message === ""){
-    //     return;
-    // }
+    // когда у нас получается new_message
+    if (data.message === ""){
+        return; // если пустое сообщение, тогда заканчиваем работу этой штуки
+    }
     io.sockets.emit("add_mes", {message: data.message, username: data.username, color: data.color})
+    // всем эмитим нашу сообщеньку
     })
 })
 
--- public/code.js
+++ public/code.js
@@ -5,14 +5,16 @@ const background_color = [
     "has-background-dark",
     "has-background-link",
 ];
+// массив наих цветов, с которыми мы работаем
 
 function getRand(arr) {
     let rand_num = Math.round(Math.random() * arr.length - 1);
     let mod_num = Math.abs(rand_num);
     return mod_num;
 }
+// просто функцтя рандомизации
 
-const socket = io.connect("http://localhost:8080/")
+const socket = io.connect("http://localhost:8080/") // подключаемся с помошью socket к нашему адресу
 
 const message = document.querySelector("#message"); // input
 const username = document.querySelector("#username"); // input под имя
@@ -21,22 +23,24 @@ const send_message = document.querySelector("#send_message"); // кнопка о
 const chat = document.querySelector("#chat"); // cам чат
 const show_name = document.querySelector("#show_name"); // отображение имени
 
-const user_color = background_color[getRand(background_color)];
+const user_color = background_color[getRand(background_color)]; // получаем случайный цвет для нашего пользователя
 
 
 function display_name(name){
+    // функция для смены нашего имени
     show_name.innerHTML  =` <span class="${user_color}"> Ваш ник ${name} </span>`
 }
-display_name("Аноним");
+display_name("Аноним"); // сначала задаем ему тип аноним
 
 send_username.onclick = () =>{
-
+    // отправляем  имя пользователя
     socket.emit("change_username", {username: username.value});
-    display_name(username.value);
+    display_name(username.value); // отрисовываем смену нашего имени
 }
 
 function sending_message(){
     socket.emit("new_message", {username: username.value, message: message.value, color: user_color});
+    //отправляем на сервер что у нас новое имя
 }
 send_message.onclick = () => {
     sending_message();
